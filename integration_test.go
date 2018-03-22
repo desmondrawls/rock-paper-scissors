0a650901368d08672a244446eaa28564a314469b
Gabe Rosenhouse <grosenhouse@pivotal.io>
2018-03-21 20:47:40 -0700

    go fmt ./...

--- integration_test.go
+++ integration_test.go
@@ -1,58 +1,58 @@
 package main_test
 
 import (
-    "fmt"
-    "os/exec"
+	"fmt"
+	"os/exec"
 
-    . "github.com/onsi/ginkgo"
-    . "github.com/onsi/gomega"
-    "github.com/onsi/gomega/gexec"
-    "github.com/sclevine/agouti"
-    . "github.com/sclevine/agouti/matchers"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gexec"
+	"github.com/sclevine/agouti"
+	. "github.com/sclevine/agouti/matchers"
 )
 
 var _ = Describe("Integration", func() {
-    var (
-        page          *agouti.Page
-        serverSession *gexec.Session
-        serverPort    string
-    )
+	var (
+		page          *agouti.Page
+		serverSession *gexec.Session
+		serverPort    string
+	)
 
-    BeforeEach(func() {
-        var err error
-        page, err = agoutiDriver.NewPage()
-        Expect(err).NotTo(HaveOccurred())
+	BeforeEach(func() {
+		var err error
+		page, err = agoutiDriver.NewPage()
+		Expect(err).NotTo(HaveOccurred())
 
-        serverPort = fmt.Sprintf("%d", PickAPort())
-        serverCmd := exec.Command(pathToServer, "--port", serverPort)
-        serverSession, err = gexec.Start(serverCmd, GinkgoWriter, GinkgoWriter)
-    })
+		serverPort = fmt.Sprintf("%d", PickAPort())
+		serverCmd := exec.Command(pathToServer, "--port", serverPort)
+		serverSession, err = gexec.Start(serverCmd, GinkgoWriter, GinkgoWriter)
+	})
 
-    AfterEach(func() {
-        Expect(page.Destroy()).To(Succeed())
-        Eventually(serverSession.Terminate).Should(gexec.Exit())
-    })
+	AfterEach(func() {
+		Expect(page.Destroy()).To(Succeed())
+		Eventually(serverSession.Terminate).Should(gexec.Exit())
+	})
 
-    assertJourney := func(player1, player2 string, expectedResult string) {
-        Expect(page.Navigate(fmt.Sprintf("http://localhost:%s", serverPort))).To(Succeed())
-        Eventually(page.FindByName("player1")).Should(BeFound())
+	assertJourney := func(player1, player2 string, expectedResult string) {
+		Expect(page.Navigate(fmt.Sprintf("http://localhost:%s", serverPort))).To(Succeed())
+		Eventually(page.FindByName("player1")).Should(BeFound())
 
-        By("allowing the user to fill out a game and submit it")
-        Expect(page.FindByName("player1").Fill(player1)).To(Succeed())
-        Expect(page.FindByName("player2").Fill(player2)).To(Succeed())
-        Expect(page.Find("input[type='submit']").Submit()).To(Succeed())
+		By("allowing the user to fill out a game and submit it")
+		Expect(page.FindByName("player1").Fill(player1)).To(Succeed())
+		Expect(page.FindByName("player2").Fill(player2)).To(Succeed())
+		Expect(page.Find("input[type='submit']").Submit()).To(Succeed())
 
-        By("showing the game result")
-        Eventually(page.HTML).Should(MatchRegexp(expectedResult))
+		By("showing the game result")
+		Eventually(page.HTML).Should(MatchRegexp(expectedResult))
 
-    }
+	}
 
-    It("should play a normal game", func() {
-        assertJourney("rock", "scissors", "player1 .* WINS")
-    })
+	It("should play a normal game", func() {
+		assertJourney("rock", "scissors", "player1 .* WINS")
+	})
 
-    It("should validate input", func() {
-        assertJourney("rock", "scissors", "player1 .* WINS")
-        //assertJourney("rock", "sailbot", "Invalid input")
-    })
+	It("should validate input", func() {
+		assertJourney("rock", "scissors", "player1 .* WINS")
+		//assertJourney("rock", "sailbot", "Invalid input")
+	})
 })
--- web_ui/handler.go
+++ web_ui/handler.go
@@ -1,14 +1,14 @@
 package web_ui
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 
-    "github.com/desmondrawls/rock-paper-scissors/play"
+	"github.com/desmondrawls/rock-paper-scissors/play"
 )
 
 const (
-    HomePageText = `<body>
+	HomePageText = `<body>
         <form action="/play" method="POST">
         <label for:"player1">P1</label>
         <input name="player1" type="string"/>
@@ -20,7 +20,7 @@ const (
         </form>
         </body>`
 
-    InvalidInputPageTemplate = `<body>
+	InvalidInputPageTemplate = `<body>
         <h1>Invalid input</h1>
         <form action="/play" method="POST">
         <label for:"player1">P1</label>
@@ -37,35 +37,35 @@ const (
 type Handler struct{}
 
 func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-    if r.URL.Path == "/" {
-        w.Write([]byte(HomePageText))
-        return
-    }
-    if r.URL.Path == "/play" && r.Method == "POST" {
-        throws := play.Inputs{
-            Player1Name:  "player1",
-            Player2Name:  "player2",
-            Player1Throw: r.FormValue("player1"),
-            Player2Throw: r.FormValue("player2"),
-        }
-        play.Play(throws, &web_ui{
-            ResponseWriter: w,
-        })
-    }
+	if r.URL.Path == "/" {
+		w.Write([]byte(HomePageText))
+		return
+	}
+	if r.URL.Path == "/play" && r.Method == "POST" {
+		throws := play.Inputs{
+			Player1Name:  "player1",
+			Player2Name:  "player2",
+			Player1Throw: r.FormValue("player1"),
+			Player2Throw: r.FormValue("player2"),
+		}
+		play.Play(throws, &web_ui{
+			ResponseWriter: w,
+		})
+	}
 }
 
 type web_ui struct {
-    http.ResponseWriter
+	http.ResponseWriter
 }
 
 func (w web_ui) Winner(name string) {
-    w.Write([]byte(fmt.Sprintf("<body>%s <br> WINS!</body>", name)))
+	w.Write([]byte(fmt.Sprintf("<body>%s <br> WINS!</body>", name)))
 }
 
 func (w web_ui) Draw() {
-    w.Write([]byte("TIE!"))
+	w.Write([]byte("TIE!"))
 }
 
 func (w web_ui) Invalid(throws play.Inputs) {
-    w.Write([]byte(fmt.Sprintf(InvalidInputPageTemplate, throws.Player1Throw, throws.Player2Throw)))
+	w.Write([]byte(fmt.Sprintf(InvalidInputPageTemplate, throws.Player1Throw, throws.Player2Throw)))
 }
